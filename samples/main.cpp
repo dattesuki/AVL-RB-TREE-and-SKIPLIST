4aa263c9845d9c705f9271f708c497338d661b3b
dattesuki <dattesuki@mail.ru>
2025-05-19 07:36:07 +0300

    first

--- samples/main.cpp
+++ samples/main.cpp
@@ -5,9 +5,20 @@
 #include "tree.h"
 #include "skiplist.h"
 #include <iostream>
+using namespace std;
 
-int main()
-{
 
-	return 0;
+
+
+
+int main() {
+    AVLTree<int> A(10,10);
+    
+    for (size_t a = 0; a < 10; a++) {
+        A.add(a, a);
+}
+    A.inorder();
+    cout << endl;
+    
+    return 0;
 }
