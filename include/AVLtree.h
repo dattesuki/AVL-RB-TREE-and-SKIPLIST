2673011d62735efdec643ddc1bee614a79928f26
dattesuki <dattesuki@mail.ru>
2025-05-23 12:33:16 +0300

    Update github_ci.yml

--- include/AVLtree.h
+++ include/AVLtree.h
@@ -5,7 +5,9 @@
 #pragma once
 #include <utility>
 #include <string>
-
+#include <algorithm>
+#include <iostream>
+using namespace std;
 
 template <typename type>
 struct NodeAVL {
@@ -115,51 +117,63 @@ NodeAVL<type>* minValueNode(NodeAVL<type>* node) {
 
 template <typename type>
 NodeAVL<type>* deleteNode(NodeAVL<type>* root, int key) {
-	if (root == nullptr)
+	if (root == nullptr) {
 		return root;
+	}
 
-	if (key < root->key)
+	if (key < root->key) {
 		root->left = deleteNode(root->left, key);
-	else if (key > root->key)
+	}
+	else if (key > root->key) {
 		root->right = deleteNode(root->right, key);
+	}
 	else {
 		if ((root->left == nullptr) || (root->right == nullptr)) {
-			NodeAVL<type>* temp = root->left ? root->left : root->right;
+			NodeAVL<type>* temp = root->left;
+			if (temp == nullptr) {
+				temp = root->right;
+			}
 
 			if (temp == nullptr) {
-				temp = root;
-				root = nullptr;
+				delete root->val;
+				delete root;
+				return nullptr;
 			}
-			else
+			else {
+				delete root->val;
 				*root = *temp;
-			delete temp->val;
-			delete temp;
+				delete temp;
+			}
 		}
 		else {
 			NodeAVL<type>* temp = minValueNode(root->right);
+			delete root->val;
 			root->key = temp->key;
-			root->val = temp->val;
+			root->val = new type(*temp->val);
 			root->right = deleteNode(root->right, temp->key);
 		}
 	}
 
-	if (root == nullptr)
+	if (root == nullptr) {
 		return root;
+	}
 
 	root->height = 1 + max(height(root->left), height(root->right));
 
 	int balance = getBalance(root);
 
-	if (balance > 1 && getBalance(root->left) >= 0)
+	if (balance > 1 && getBalance(root->left) >= 0) {
 		return rightRotate(root);
+	}
 
 	if (balance > 1 && getBalance(root->left) < 0) {
 		root->left = leftRotate(root->left);
 		return rightRotate(root);
 	}
 
-	if (balance < -1 && getBalance(root->right) <= 0)
+	if (balance < -1 && getBalance(root->right) <= 0) {
 		return leftRotate(root);
+	}
 
 	if (balance < -1 && getBalance(root->right) > 0) {
 		root->right = rightRotate(root->right);
@@ -171,17 +185,19 @@ NodeAVL<type>* deleteNode(NodeAVL<type>* root, int key) {
 
 template <typename type>
 bool findTree(NodeAVL<type>* root, int key) {
-	if (root->key == key) return true;
+	if (root == nullptr) {
+		return false;
+	}
+	if (root->key == key) {
+		return true;
+	}
 	if (key < root->key) {
-		if (root->left == nullptr) return false;
 		return findTree<type>(root->left, key);
 	}
-	if (key > root->key) {
-		if (root->right == nullptr) return false;
+	else {
 		return findTree<type>(root->right, key);
 	}
 }
-
 
 template <typename type>
 void inorderTree(NodeAVL<type>* root) {
--- samples/main.cpp
+++ samples/main.cpp
@@ -13,12 +13,30 @@ using namespace std;
 
 int main() {
     AVLTree<int> A(10,10);
-    
-    for (size_t a = 0; a < 10; a++) {
+    SkipList<int> B;
+//    for (size_t a = 0; a < 15; a++) {
+//        A.add(a, a);
+//        B.add(a, a);
+//}
+    for (int a = 15; a >=0; --a) {
         A.add(a, a);
-}
-    A.inorder();
-    cout << endl;
+        B.add(a, a);
+    }
+    //A.inorder();
+    //cout << endl;
+    //
     
+    B.print();
+    cout << endl;
+    for (size_t i = 6; i < 8; ++i) B.remove(i);
+    B.print();
+    
+
+
+
+
     return 0;
 }
+
+
+
